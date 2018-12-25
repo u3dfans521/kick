82d67fe8ed2cfc7c26e4e826c5ac20df739e5a8f
ue4-fans-code521 <kongziliang714@163.com>
2018-12-25 14:52:25 +0800

    修改之后.....

---
+++ src/Final.java
@@ -0,0 +1,7 @@
+import java.sql.SQLOutput;
+
+public class Final {
+    public static void main(String[] args) {
+        System.out.println("金蝶的完成");
+    }
+}
--- src/Pj.java
+++ src/Pj.java
@@ -2,5 +2,7 @@ public class Pj {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("我不是药神");
+        System.out.println("图悠然");
+        System.out.println("南瑟吉欧");
     }
 }
